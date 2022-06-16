c3d1021fb50633d67bc8a575b81449a6e99dde8e
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-06-16 17:18:41 +0300

    commit tree 62f9f6a857665f724b8147cf2f1e7dcaf2755f53

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify({
+                name: userName,
+                email: userEmail
+            })
+        })
+        .then(response => response.json())
+        .then(userData => {
+            const id = userData.id
+            document.querySelector("body").innerHTML = `
+            ${id}
+        `
+        })
+        .catch(function(error) {
+            document.querySelector("body").innerHTML = `
+            ${error.message}
+       `
+        })
+}
